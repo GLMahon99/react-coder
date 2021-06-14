59f55d4342cd04f6ee887595b016bee0da4e563e
gaston_mahon <gastimahon@gmail.com>
2021-06-14 20:47:07 -0300

    agrego navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function NavBar(){
+    return(
+        <div className="navbar">
+            <ul>
+                <li>Menú</li>
+                <li>Productos</li>
+                <li>Nosotros</li>
+                <li>Contacto</li>
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar;
