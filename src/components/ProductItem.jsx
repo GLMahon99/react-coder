825ee5d8929b70a4158ebf96fb328c8c418d7eb0
gaston_mahon <gastimahon@gmail.com>
2021-07-17 18:08:37 -0300

    realizo correcciones

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -2,20 +2,32 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const ProductItem = (titulo, img) => {
+const styleTitle = {
+    color: "gray",
+    fontSize: "14px",
+    fontStyle: "lighter",
+    
+
+}
+
+const ProductItem = ({titulo, img}) => {
     return ( 
         <>
-            <Link to={`/products/${1}`}>
+            { !titulo && !img ? (
+                <p>cargando productos</p>
+            ) : (
+            <Link style={{textDecoration: "none"}} to={`/products/${1}`}>
             <div className="col my-2">
                     <div className="card">
-                        <img src={img} class="card-img-top" alt="..."/>
+                        <img src={img} className="card-img-top" alt="..."/>
                         <div className="card-body">
-                            <h5 className="card-title">{titulo}</h5>
+                            <h5 className="card-title" style={styleTitle}>{titulo}</h5>
                         </div>
                         
                     </div>
                 </div>
-            </Link>    
+            </Link>   
+            )} 
         </>
      );
 }
